ed7006f1b08728e83c453c2738d3ee962bd325ba
NYatsenko <noyatsenko@ya.ru>
2019-03-01 13:11:10 +0300

    Упрощена логика вставки приоритетного дека. Настолько упрощена, что javadoc больше не нужен.

--- JavaАlg1Lesson3/src/JavaАlg1Lesson3/MyDequePrior.java
+++ JavaАlg1Lesson3/src/JavaАlg1Lesson3/MyDequePrior.java
@@ -14,37 +14,56 @@ public class MyDequePrior extends MyDeque {
 
     @Override
     public void add(char ch){
-        if (size == 0) { //если это будет самый первый элемент
+        if (pLast == null) { //если элементов нет
             pFirst = pLast = new Deque(null,null,ch);
         } else{
             Deque pCheck = pLast; //создаем временный указатель
             while (pCheck.ch > ch && pCheck.first != null) { pCheck = pCheck.first; } //ищем элемент меньше
 
-            /**
-             * Пояснения к вставке второго и далее элементов
-             * 1. Если вставляем в середине очереди после pCheck, где существуют соседи - все просто.
-             * 2. Иначе проверяем: если элемент самый первый и больше - надо вставлять перед pCheck.
-             * 3. Иначе следующий простой случай - а вдруг мы вставляем за последим элементом?
-             * 4. Теперь мы знаем, что вставляем сразу за первым элементом, у которого есть last,
-             *    обработанный в 3. Для наглядности оставил ненужный if.
-             */
-            if (pCheck.last != null && pCheck.first != null) {
+            //сначала граничные случаи
+            if (pCheck.first == null && pCheck.ch > ch){        // если вставляем перед первым элементом
+                pFirst = new Deque(null, pCheck,ch);
+                pCheck.first = pFirst;
+            } else if (pCheck.last == null){                    // иначе если вставляем после первого или другого элемента
+                pLast = new Deque(pCheck,null,ch);
+                pCheck.last = pLast;
+            } else {                                            //иначе общий случай
                 // Все просто:
                 // 1. pCheck.last - элемент, который мы только что вставили
                 // 2. pCheck.last.last - элемент после только что вставленного элемента
-                // 3. pChech.last.last.first - очевидно, 2. должен ссылаться на элемент перед 2. - а это 1.
+                // 3. pCheck.last.last.first - очевидно, 2. должен ссылаться на элемент перед 2. - а это 1.
                 pCheck.last = new Deque(pCheck, pCheck.last,ch);
                 pCheck.last.last.first = pCheck.last;
-            } else if (pCheck.first == null && pCheck.ch > ch){
-                pFirst = new Deque(null, pCheck,ch);
-                pCheck.first = pFirst;
-            } else if (pCheck.last == null){
-                pLast = new Deque(pCheck,null,ch);
-                pCheck.last = pLast;
-            } else if (pCheck.first == null && pCheck.last != null) {
-                pCheck.last = new Deque(pCheck, pCheck.last, ch);
-                pCheck.last.last.first = pCheck.last;
             }
+
+//            Предыдущая реализация логики вставки, переусложнена (очевидно, раз 1 и 4 совпадают).
+//            Не уверен, что работает быстрее, хотя в этом и был смысл такой реализации.
+//            Оставлю тут в учебных целях "как бывает сложно аж до javadoc."
+//            /**
+//             * Пояснения к вставке второго и далее элементов
+//             * 1. Если вставляем в середине очереди после pCheck, где существуют соседи - все просто.
+//             * 2. Иначе проверяем: если элемент самый первый и больше - надо вставлять перед pCheck.
+//             * 3. Иначе следующий простой случай - а вдруг мы вставляем за последим элементом?
+//             * 4. Теперь мы знаем, что вставляем сразу за первым элементом, у которого есть last,
+//             *    обработанный в 3. Для наглядности оставил ненужный if.
+//             */
+//            if (pCheck.last != null && pCheck.first != null) {
+//                // Все просто:
+//                // 1. pCheck.last - элемент, который мы только что вставили
+//                // 2. pCheck.last.last - элемент после только что вставленного элемента
+//                // 3. pChech.last.last.first - очевидно, 2. должен ссылаться на элемент перед 2. - а это 1.
+//                pCheck.last = new Deque(pCheck, pCheck.last,ch);
+//                pCheck.last.last.first = pCheck.last;
+//            } else if (pCheck.first == null && pCheck.ch > ch){
+//                pFirst = new Deque(null, pCheck,ch);
+//                pCheck.first = pFirst;
+//            } else if (pCheck.last == null){
+//                pLast = new Deque(pCheck,null,ch);
+//                pCheck.last = pLast;
+//            } else if (pCheck.first == null && pCheck.last != null) {
+//                pCheck.last = new Deque(pCheck, pCheck.last, ch);
+//                pCheck.last.last.first = pCheck.last;
+//            }
         }
         size++;
     }
